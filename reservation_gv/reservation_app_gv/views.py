83780343e2dbcde5035d60860c62a9cd910a0b5e
Tomfif <rarok.tomasz@gmail.com>
2020-09-11 17:49:05 +0200

    Commit 2.1

--- reservation_gv/reservation_app_gv/views.py
+++ reservation_gv/reservation_app_gv/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView
-
 from reservation_app_gv.models import ConferenceRoom
 
 
